e65f275e2cb06eda371ee0ab9ea846f596d103ae
Orel <Orely4242@gmail.com>
2022-02-05 16:34:45 +0200

    d2x: adapt for PixelExperience

---
+++ aosp_d2x.mk
@@ -0,0 +1,35 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+
+
+## Inherit from generic products, most specific first
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+## Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
+## Inherit from d2x device
+$(call inherit-product, device/samsung/d2x/device.mk)
+
+## Inherit some common build stuff
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+## Boot Animation
+TARGET_BOOTANIMATION_HALF_RES := true
+TARGET_SCREEN_HEIGHT := 3040
+TARGET_SCREEN_WIDTH := 1440
+TARGET_HAVE_FOD := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+## Device identifier, this must come after all inclusions
+PRODUCT_NAME := aosp_d2x
+PRODUCT_DEVICE := d2x
+PRODUCT_BRAND := Samsung
+PRODUCT_MODEL := Galaxy Note10+ 5G
+PRODUCT_MANUFACTURER := Samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
